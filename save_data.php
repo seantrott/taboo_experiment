fdd5c6e3dccbf055d63096e8a664e76b262897cf
Sean Trott <seantrott@Seans-MacBook-Pro-6.local>
2019-03-04 17:30:48 -0800

    update

--- save_data.php
+++ save_data.php
@@ -1,7 +1,7 @@
 <?php
 // the $_POST[] array will contain the passed in filename and data
 // the directory "data" is writable by the server (chmod 777)
-$filename = "/home/web/lcl/www-data/Taboo".$_POST['filename'];
+$filename = "/home/web/lcl/www-data/Taboo/pilot/".$_POST['filename'];
 $data = $_POST['filedata'];
 // write the file to disk
 file_put_contents($filename, $data);
